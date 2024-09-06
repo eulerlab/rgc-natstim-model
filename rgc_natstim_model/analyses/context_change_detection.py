fab17f3c34167effb97f2fbbf534ea5f5cfcb8a8
Lara Hoefling <46810932+lhoefling@users.noreply.github.com>
2024-09-06 14:44:11 +0200

    Add documentation (#7)
    Change identity

--- rgc_natstim_model/analyses/context_change_detection.py
+++ rgc_natstim_model/analyses/context_change_detection.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from scipy import interpolate
 import pickle as pkl
-from typing import Dict
+from typing import Dict, Union, Tuple
 from rgc_natstim_model.constants.context_change_detection import (
     DUR_MOVIE, RANDOM_SEQUENCES_PATH, GROUP_INFO_PATH, START_INDICES,
     NUM_CLIPS, NUM_CLIPS_TOTAL, NUM_VAL_CLIPS, CLIP_DUR, MOVIE_DUR
@@ -14,13 +14,19 @@ from rgc_natstim_model.constants.context_change_detection import (
 def get_resp_dicts(dataframe: pd.DataFrame,
                    reduce_fn=None,
                    normalize_fn=None):
-    '''
+    
+    """
+    Given a dataframe indexed by neuron_id and with a column 'responses_final' containing the responses
+    of neurons to the movie, this function returns two dictionaries (indexed by neuron_id): one with the 
+    full responses, and one with the responses, transformed by the given reduce_fn and normalize_fn, for
+    the time points given by START_INDICES. In the default configuration, this will return the mean z-scored
+    response of the neuron to the 30 frames following each time point in START_INDICES.
 
-    :param dataframe:
-    :param reduce_fn:
-    :param normalize_fn:
-    :return:
-    '''
+    :param dataframe: pd.DataFrame
+    :param reduce_fn: function to reduce the responses, defaulting to the mean of the 30 frames following each time point
+    :param normalize_fn: function to normalize the responses, defaulting to z-scoring
+    :return: full_movie_resp_dict, binned_movie_resp_dict
+    """
     rnd = np.random.RandomState(seed=1000)
     val_clip_idx = list(rnd.choice(NUM_CLIPS, NUM_VAL_CLIPS, replace=False))
     train_clip_idx = list(np.arange(NUM_CLIPS))
@@ -50,12 +56,28 @@ def get_resp_dicts(dataframe: pd.DataFrame,
 
 
 def get_movie_contrast_by_session(session_ids, sess2movie_dict):
-    movie_contrasts_session_ff = {session_id: np.zeros((2, len(START_INDICES)))
+    """
+    Calculate the movie contrasts for each session.
+    Parameters:
+    - session_ids (array-like): An array-like object containing the session IDs.
+    - sess2movie_dict (dict): A dictionary mapping session IDs to movie data.
+    Returns:
+    - movie_contrasts_by_session (dict): A dictionary mapping session IDs to movie contrasts.
+        Each movie contrast is a 2D array of shape (2, len(START_INDICES)).
+        The first dimension represents the movie channel (0 or 1),
+        and the second dimension represents the movie contrast for each start index.
+    """
+    # Function body...
+
+
+
+    movie_contrasts_by_session = {session_id: np.zeros((2, len(START_INDICES)))
                                   for session_id in np.unique(session_ids)}
 
     for sid in np.unique(session_ids):
         temp_movie = sess2movie_dict[sid]
-        movie_contrasts_session_ff[sid][0] = np.asarray(
+        # green channel
+        movie_contrasts_by_session[sid][0] = np.asarray(
             [np.mean(
                 temp_movie[0, i, :30],
                 axis=(-1, -2, -3)
@@ -65,7 +87,8 @@ def get_movie_contrast_by_session(session_ids, sess2movie_dict):
             )
              for i in range(1, len(START_INDICES) + 1)])
 
-        movie_contrasts_session_ff[sid][1] = np.asarray(
+        # UV channel
+        movie_contrasts_by_session[sid][1] = np.asarray(
             [np.mean(
                 temp_movie[1, i, :30],
                 axis=(-1, -2, -3)
@@ -74,12 +97,37 @@ def get_movie_contrast_by_session(session_ids, sess2movie_dict):
                 axis=(-1, -2, -3)
             )
              for i in range(1, len(START_INDICES) + 1)])
-    return movie_contrasts_session_ff
+    return movie_contrasts_by_session
 
 
 class SessionMappings:
+
     def __init__(self, train_movie_snippets, test_movie_snippets,
                  random_sequences_path, group_info_path, dur_movie):
+        """
+        Class representing session mappings.
+
+        Attributes:
+            train_movie_snippets              (numpy.ndarray): Array of train movie snippets.
+            test_movie_snippets               (numpy.ndarray): Array of test movie snippets.
+            RANDOM_SEQUENCES_PATH             (str)          : Path to random sequences.
+            GROUP_INFO_PATH                   (str)          : Path to group info.
+            DUR_MOVIE                         (int)          : Duration of the movie.
+            sess2seq_test                     (dict)         : Dictionary mapping session IDs to test sequences.
+            sess2seqid                        (dict)         : Dictionary mapping session IDs to sequence IDs.
+            sess2seq                          (dict)         : Dictionary mapping session IDs to sequences.
+            sess2vertical_transitions_dict    (dict)         : Dictionary mapping session IDs to vertical transition types.
+            sess2brightness_transitions_dict  (dict)         : Dictionary mapping session IDs to brightness classes.
+            sess2movie_dict                   (dict)         : Dictionary mapping session IDs to movies.
+            sess2flatmovie_dict               (dict)         : Dictionary mapping session IDs to flattened movies.
+            sess2train_movie_dict             (dict)         : Dictionary mapping session IDs to train movies.
+            sess2flattrain_movie_dict         (dict)         : Dictionary mapping session IDs to flattened train movies.
+
+        Methods:
+            load_sequences_info()                                 : Load sequences information from files.
+            get_session_mappings(movie_data_dicts, dh_2_session_ids): Get session mappings.
+        """
+
         self.sess2seq_test = {}
         self.sess2seqid = {}
         self.sess2seq = {}
@@ -145,22 +193,26 @@ class SessionMappings:
 
 
 ######################################### ROC analyses #################################################################
-def get_roc_curve(responses,
-                  transitions,
-                  target_transition,
-                  offtarget_transition,
-                  above_threshold,
-                  num_bins=40):
-    """
-    Calculate ROC curve based on binned responses
 
-    :param responses:
-    :param transitions:
-    :param target_transition:
-    :param offtarget_transition:
-    :param above_threshold:
-    :param num_bins:
-    :return:
+def get_roc_curve(responses: np.ndarray,
+                  transitions: np.ndarray,
+                  target_transition: Union[int, str],
+                  offtarget_transition: Union[int, str],
+                  above_threshold: bool,
+                  num_bins: int = 40) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Individual cell level function for calculating Receiver Operating Characteristics curve based on binned responses.
+
+    Parameters:
+        responses            (numpy.ndarray): Array of response values.
+        transitions          (numpy.ndarray): Array of transition labels.
+        target_transition    (int or str)   : Label of the target transition.
+        offtarget_transition (int, str, or "all"): Label of the off-target transition or "all" for all non-target transitions.
+        above_threshold      (bool)         : Whether to consider responses above/below threshold as positive prediction.
+        num_bins             (int)          : Number of bins for sampling points on the response curve (default is 40).
+
+    Returns:
+        tuple: A tuple containing the false positive rates, true positive rates, and thresholds.
     """
     ## get the indexes for target and non-target transitions
     positives_bool = transitions == target_transition
@@ -185,21 +237,31 @@ def get_roc_curve(responses,
     return fpr, tpr, f, f_inverse, auc, ds
 
 
-def get_ind_roc_curve(df, sess2vertical_transitions, binned_resp_dict, target_transition,
-                      cell_type, offtarget_transition="all",
-                      num_bins=40, above_threshold=True):
+def get_ind_roc_curve(df: pd.DataFrame,
+                      sess2vertical_transitions: Dict[str, np.ndarray],
+                      binned_resp_dict: Dict[str, np.ndarray],
+                      target_transition: Union[int, str],
+                      cell_type: str,
+                      offtarget_transition: Union[int, str] = "all",
+                      num_bins: int = 40,
+                      above_threshold: bool = True):
+    """
+    Calls get_roc_curve for each neuron of type cell_type in the DataFrame and returns the results in dictionaries.
+
+    Parameters:
+        df                     (pd.DataFrame)          : DataFrame containing neuron and session information.
+        sess2vertical_transitions (dict)               : Dictionary mapping session IDs to transition labels.
+        binned_resp_dict       (dict)                  : Dictionary mapping neuron IDs to binned response values.
+        target_transition      (int or str)            : Label of the target transition.
+        cell_type              (str)                   : Cell type to filter neurons.
+        offtarget_transition   (int, or "all")         : Label of the off-target transition or "all" for all non-target transitions (default is "all").
+        num_bins               (int)                   : Number of bins for sampling points on the response curve (default is 40).
+        above_threshold        (bool)                  : Whether to consider responses above/below threshold as positive prediction.
+
+    Returns:
+        tuple: A tuple containing dictionaries for true positive rates, false positive rates, AUC values, ROC curve functions, inverse ROC curve functions, and thresholds by neuron ID.
     """
 
-    :param df:
-    :param sess2vertical_transitions:
-    :param binned_resp_dict:
-    :param target_transition:
-    :param cell_type:
-    :param offtarget_transition:
-    :param num_bins:
-    :param above_threshold:
-    :return:
-    """
     tpr_by_nid = {}
     fpr_by_nid = {}
     auc_by_nid = {}
