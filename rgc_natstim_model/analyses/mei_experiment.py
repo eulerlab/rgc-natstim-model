db14e53268c64e1083d4e0c5eea9bba409f6917a
Larissa Hoefling <larissa.hoefling@posteo.de>
2024-09-25 15:52:59 +0200

    Update documentation

--- rgc_natstim_model/analyses/mei_experiment.py
+++ rgc_natstim_model/analyses/mei_experiment.py
@@ -1,6 +1,24 @@
 import numpy as np
 
 def get_weighted_resp(df, neuron_id, col_key="resp_per_loc"):
+    """
+    Get the spatially weighted response of a neuron to the MEI stimuli.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame containing the responses and spatial weighting functions.
+    neuron_id : int
+        Index of the neuron in the DataFrame.
+    col_key : str
+        Key for the column containing the responses (recorded or model responses).
+
+    Returns
+    -------
+    resp : np.ndarray
+        Weighted response of the neuron.
+    """
+
     weights = df["spatial_weighting_function"].loc[neuron_id]
     normed_weights = weights / weights.sum()
 
